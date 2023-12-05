4227a673003f939a398bda6466bade1edaaf982f
Amira Ahmed Trainer <97535863+Amytrainer@users.noreply.github.com>
2023-12-05 19:49:42 +0000

    Add files via upload

---
+++ MenuCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function MenuCard () {  
+
+          return (
+            <>
+              <div className="card-container" >
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle"></span>
+                    <span className="card-author subtle"> </span>
+                    <h2 className="card-title">  </h2>
+                    <span className="card-description subtle">
+                    </span>
+                    <div className="card-read"></div>
+                  </div>
+                  <img src="" alt="images" className="card-media" />
+
+                  <span className="card-tag  subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        
+     
+};
+
+export default MenuCard;
---
+++ Resturant.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+import Menu from "./menuApi";
+import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
+
+function Resturant  ()  {
+
+  return (
+    <>
+      <Navbar  />
+      <MenuCard  />
+    </>
+  );
+};
+
+export default Resturant;
