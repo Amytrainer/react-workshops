92c7d849165a37dbd0b292d5080bbaf5f28625f3
Amira Ahmed Trainer <97535863+Amytrainer@users.noreply.github.com>
2023-12-05 20:09:30 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant.jsx";
+
+
+function App () {
+  return  (
+    <>
+    <navbar />,
+    <Resturant />
+    </>);
+  
+};
+
+export default App;
